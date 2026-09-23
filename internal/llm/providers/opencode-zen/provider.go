4632bc3b4d1d6b0cbdd73fef82dfe6d1a413b3f8
Ömer Faruk Demirel <omerfdmrl@gmail.com>
2026-09-23 15:11:48 +0300

    fix: update zen to use dynamic random headers

--- internal/llm/providers/opencode-zen/provider.go
+++ internal/llm/providers/opencode-zen/provider.go
@@ -1,12 +1,14 @@
 package opencodezen
 
 import (
+	"fmt"
 	"maps"
 	"time"
 
 	"github.com/vesvai/vesvai/internal/core/config"
 	"github.com/vesvai/vesvai/internal/llm"
 	openaidriver "github.com/vesvai/vesvai/internal/llm/drivers/openai"
+	"github.com/vesvai/vesvai/internal/utils/random"
 )
 
 const (
@@ -20,11 +22,11 @@ func init() {
 
 func NewFromConfig(cfg config.LLMConfig) (llm.Provider, error) {
 	defaultHeaders := map[string]string{
-		"User-Agent":         "opencode/1.18.29 ai-sdk/provider-utils/4.0.23 runtime/bun/1.3.14",
-		"x-opencode-client":  "vesvai",
-		"x-opencode-project": "vesvai",
-		"x-opencode-request": "vesvai",
-		"x-opencode-session": "vesvai",
+		"User-Agent":         "opencode/1.18.31 ai-sdk/provider-utils/4.0.23 runtime/bun/1.3.14",
+		"x-opencode-client":  "cli",
+		"x-opencode-project": random.GenerateHex(20),
+		"x-opencode-request": fmt.Sprintf("msg_%s", random.GenerateAlphanumeric(26)),
+		"x-opencode-session": fmt.Sprintf("ses_%s%s", random.GenerateHex(12), random.GenerateAlphanumeric(14)),
 	}
 	maps.Copy(defaultHeaders, cfg.Headers)
 	return openaidriver.NewService(ProviderName, openaidriver.ServiceConfig{
